4c845613124b8c82981eb9a3c314dd8f66d46bc2
Matthew Lu <seedmachinegun@gmail.com>
2022-07-12 16:29:27 +0100

    Created a basic empty table

--- src/lib/SpreadSheet.tsx
+++ src/lib/SpreadSheet.tsx
@@ -2,20 +2,34 @@ import React from "react"
 import "./SpreadSheet.css"
 
 function SpreadSheet() {
+  let numCol = 26
+  let numRow = 50
   return (
-    <table>
-      <tr>
-        <th>Data 1</th>
-        <th>Data 2</th>
-      </tr>
-      <tr>
-        <td>Calcutta</td>
-        <td>Orange</td>
-      </tr>
-      <tr>
-        <td>Robots</td>
-        <td>Jazz</td>
-      </tr>
+    <table className="SpreadSheet">
+      <thead>
+        <tr>
+          {Array(numCol + 1)
+            .fill(null)
+            .map((_, idx) => {
+              if (idx === 0) return <th></th>
+              return <th key={idx}>{String.fromCharCode(64 + idx)}</th>
+            })}
+        </tr>
+      </thead>
+      <tbody>
+        {Array(numRow)
+          .fill(null)
+          .map((_, rowIdx) => (
+            <tr key={rowIdx}>
+              {Array(numCol + 1)
+                .fill(null)
+                .map((_, colIdx) => {
+                  if (colIdx === 0) return <th>{rowIdx + 1}</th>
+                  return <td key={colIdx}></td>
+                })}
+            </tr>
+          ))}
+      </tbody>
     </table>
   )
 }
