ffe7498ad4e1d5904bf4384c1bacd0a1ae13f622
Matthew Lu <seedmachinegun@gmail.com>
2022-07-12 15:34:43 +0100

    Added Prettier config and added SpreadSheet component

---
+++ src/lib/SpreadSheet.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import "./SpreadSheet.css"
+
+function SpreadSheet() {
+  return (
+    <table>
+      <tr>
+        <th>Data 1</th>
+        <th>Data 2</th>
+      </tr>
+      <tr>
+        <td>Calcutta</td>
+        <td>Orange</td>
+      </tr>
+      <tr>
+        <td>Robots</td>
+        <td>Jazz</td>
+      </tr>
+    </table>
+  )
+}
+
+export default SpreadSheet
